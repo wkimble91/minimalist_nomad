c94db1ff17866ecd6c222fcc98faafbace0fca78
Will <wkimble91@gmail.com>
2023-05-06 17:19:42 -0500

    navbar now pops out on mobile

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -6,24 +6,21 @@ const navLink = document.querySelectorAll('.header__nav-item');
 hamburger.addEventListener('click', mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 }
 
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }
 
 // Scrolled class to shrink nav and apply background
 $(function () {
-    $(document).scroll(function () {
-        var $nav = $('#header');
-        $nav.toggleClass(
-            'header-scrolled',
-            $(this).scrollTop() > $nav.height()
-        );
-    });
+  $(document).scroll(function () {
+    var $nav = $('#header');
+    $nav.toggleClass('header-scrolled', $(this).scrollTop() > $nav.height());
+  });
 });
